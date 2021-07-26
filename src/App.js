772fb9385331fc4c2cd51cbefb9fd14db9ad5339
clintonngotta <clintonngotta@gmail.com>
2021-07-26 06:43:58 +0300

    design

--- src/App.js
+++ src/App.js
@@ -1,16 +1,143 @@
 import React, { Component } from "react";
 import "./App.css";
-import Login from "./components/Login";
-// import Register from "./components/Register";
-// import ResetPassword from "./components/ResetPassword";
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: "login",
+    };
+  }
   render() {
-    return (
-      <div className="wrap">
-        <Login />
-        {/* <Register />
-        <ResetPassword /> */}
-      </div>
-    );
+    const { page } = this.state;
+    let formElements = "";
+    if (page === "login") {
+      formElements = (
+        <div id="login">
+          <h1>Login</h1>
+          <label htmlFor="uname">
+            <b>Username</b>
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Enter Username"
+            name="username"
+            required
+          />
+
+          <label htmlFor="psw">
+            <b>Password</b>
+          </label>
+          <input
+            className="form-control"
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            required
+          />
+
+          <button
+            type="button"
+            className="btn btn-outline-primary form-control"
+          >
+            <i className="bi bi-box-arrow-in-right"></i> Login
+          </button>
+          <div
+            style={{
+              marginTop: "10px",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <button className="btn btn-outline-primary">
+              <i className="bi bi-arrow-right-circle"></i> Register
+            </button>
+            <button className="btn btn-outline-primary">
+              <i className="bi bi-unlock"></i> Forgot Password
+            </button>
+          </div>
+        </div>
+      );
+    } else if (page === "register") {
+      formElements = (
+        <div id="register">
+          <h1>Register</h1>
+          <label htmlFor="uname">
+            <b>Username</b>
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Enter Username"
+            name="username"
+            required
+          />
+
+          <label htmlFor="psw">
+            <b>Password</b>
+          </label>
+          <input
+            className="form-control"
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            required
+          />
+
+          <button
+            type="button"
+            className="btn btn-outline-primary form-control"
+          >
+            <i className="bi bi-arrow-right-circle"></i> Register
+          </button>
+          <div
+            style={{
+              marginTop: "10px",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <button className="btn btn-outline-primary">
+              <i className="bi bi-arrow-left-circle"></i> Back To Login
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      formElements = (
+        <div id="forgotpassword">
+          <h1>Reset Password</h1>
+          <label htmlFor="uname">
+            <b>Email</b>
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Enter Email"
+            name="email"
+            required
+          />
+          <button
+            type="button"
+            className="btn btn-outline-primary form-control"
+          >
+            <i className="bi bi-unlock"></i> Reset
+          </button>
+          <div
+            style={{
+              marginTop: "10px",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <button className="btn btn-outline-primary">
+              <i className="bi bi-arrow-left-circle"></i> Back
+            </button>
+          </div>
+        </div>
+      );
+    }
+
+    return <div className="wrap">{formElements}</div>;
   }
 }
